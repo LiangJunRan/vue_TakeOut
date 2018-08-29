d00566057ba22a36e69dbf38e78589a3dc66af95
LiangJunRan <254092895@qq.com>
2018-08-29 19:49:57 +0800

    首页、探索、订单、我的、注册静态页面完毕

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,26 @@
 <template>
   <div id="app">
-   
+		<router-view></router-view>
+		<FooterGuide v-show="$route.meta.showFooter"></FooterGuide>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'App'
+	
+	import FooterGuide from './components/FooterGuide/FooterGuide.vue'
+	
+	export default {
+		
+	components:{
+		FooterGuide
+	},
+		
 }
 </script>
 
-<style>
-
+<style lang="stylus" rel="stylesheet/stylus">
+	#app
+		width 100%
+		height 100%
+		background #f5f5f5
 </style>
